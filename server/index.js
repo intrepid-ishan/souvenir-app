51626246c7a6752077f7b098f553d811ed6ec6e6
Ishan Makadia <coderishan@gmail.com>
2020-11-09 12:46:48 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+dotenv.config();
+
+const CONNECTION_URL = process.env.DATABASE;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
